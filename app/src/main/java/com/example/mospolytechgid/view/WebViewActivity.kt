8d986312b355397f0659fb7da197ca7bdab2cd82
Руслан Асадов <rustv2002@mail.ru>
2024-02-25 22:26:00 +0300

    Update WebView control

--- app/src/main/java/com/example/mospolytechgid/view/WebViewActivity.kt
+++ app/src/main/java/com/example/mospolytechgid/view/WebViewActivity.kt
@@ -1,10 +1,15 @@
 package com.example.mospolytechgid.view
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mospolytechgid.R
 
@@ -13,16 +18,71 @@ class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+        // поиск и присвоение WebView переменной
+        val webView: WebView = findViewById(R.id.webView);
+        // обработка события "назад"
+        val callback = object : OnBackPressedCallback(true) {
+            var isExit = false
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    isExit = false
+                    webView.goBack()
+                } else {
+                    if (isExit)
+                        finish()
+                    else {
+                        Toast.makeText(
+                            baseContext,
+                            "Повторите действие чтобы выйти",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        isExit = true
+                    }
+                }
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, callback)
         // скрытие заголовка приложения
         supportActionBar?.hide()
-
-        val webView: WebView = findViewById(R.id.webView)
-        // использование кэша при оттсутвии интернета
+        // использование кэша при отсутствии интернета
         webView.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
         // использование js
         webView.settings.javaScriptEnabled = true
-        // создание клиента, чтобы не исользовались приложения по умолчанию
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://mospolynavigation.github.io/WebSite/")
+        // разрешить сайтам доступ к хранилищу
+        webView.settings.domStorageEnabled = true
+        // смена масштаба
+        webView.settings.useWideViewPort = true
+        webView.setInitialScale(1);
+        // создание web-клиента, чтобы переопределить его работу
+        webView.webViewClient = object : WebViewClient() {
+            // перегрузка загрузки сайта
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                return if (request.url.host != "mospolynavigation.github.io") {
+                    val intent = Intent(Intent.ACTION_VIEW, request.url)
+                    startActivity(intent)
+                    true
+                } else false
+            }
+            // обработка ошибок при загрузке
+            override fun onReceivedError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
+                try {
+                    webView.stopLoading()
+                } catch (e: java.lang.Exception) {
+                    e.printStackTrace()
+                }
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                }
+                Toast.makeText(baseContext, "Ошибка", Toast.LENGTH_LONG).show()
+            }
+        }
+        webView.loadUrl("https://mospolynavigation.github.io/dod/")
     }
 }
